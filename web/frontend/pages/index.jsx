a7bd62d5518bd289089a2c33a528fb35e1ddc01a
Guy Heidemann <guyheidemann@gmail.com>
2022-09-21 14:57:03 -0400

    updates

--- web/frontend/pages/index.jsx
+++ web/frontend/pages/index.jsx
@@ -6,6 +6,7 @@ import {
   Page,
   SkeletonBodyText,
 } from "@shopify/polaris";
+
 export default function HomePage() {
   /*
     Add an App Bridge useNavigate hook to set up the navigate function.
@@ -17,7 +18,7 @@ export default function HomePage() {
   /*
     These are mock values. Setting these values lets you preview the loading markup and the empty state.
   */
-  const isLoading = true;
+  const isLoading = false;
   const isRefetching = false;
   const QRCodes = [];
 
